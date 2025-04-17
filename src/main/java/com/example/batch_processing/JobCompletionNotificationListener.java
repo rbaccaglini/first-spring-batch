28eafa3e2e05feb885522e85fe8993077836b436
Rogério Baccaglini <56352335+rbaccaglini@users.noreply.github.com>
2025-04-17 10:05:36 -0300

    select data to app people table

--- src/main/java/com/example/batch_processing/JobCompletionNotificationListener.java
+++ src/main/java/com/example/batch_processing/JobCompletionNotificationListener.java
@@ -2,10 +2,10 @@ package com.example.batch_processing;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -15,10 +15,10 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate applicationJdbcTemplate;
 
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(@Qualifier("applicationJdbcTemplate") JdbcTemplate jdbcTemplate) {
+        this.applicationJdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -26,7 +26,7 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
 
-            jdbcTemplate
+            applicationJdbcTemplate
                     .query("SELECT first_name, last_name FROM people", new DataClassRowMapper<>(Person.class))
                     .forEach(person -> log.info("Found <{}> in the database.", person));
         }
