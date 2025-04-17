3fdd79e8e7169b4f40df2895dbbf333d7425973a
Rogério Baccaglini <56352335+rbaccaglini@users.noreply.github.com>
2025-04-17 10:10:19 -0300

    Data separation
    * adding app db - application.properties
    * writer pointing to app db
    * config ds app db and Jdbc template
    * select data to app people table

--- src/main/java/com/example/batch_processing/DataSourceConfig.java
+++ src/main/java/com/example/batch_processing/DataSourceConfig.java
@@ -1,10 +1,12 @@
 package com.example.batch_processing;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -16,6 +18,18 @@ public class DataSourceConfig {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource springDS() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource applicationDS() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Qualifier("applicationJdbcTemplate")
+    @Bean
+    public JdbcTemplate applicationJdbcTemplate(@Qualifier("applicationDS") DataSource applicationDS) {
+        return new JdbcTemplate(applicationDS);
     }
 
 }
