6c6cb2d980820aeeaa0fb379c310065a5f5220ba
Rogério Baccaglini <56352335+rbaccaglini@users.noreply.github.com>
2025-04-17 09:59:39 -0300

    Add database mysql
    Using Mysql as a database.
    * Create docker-compose.yml
    * Update pom.xml
    * Create DataSourceConfig.java
    * Update application.properties

---
+++ src/main/java/com/example/batch_processing/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.example.batch_processing;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource springDS() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
